9b6de4eeb24376b6fe2818a56fdd953b17e34de8
Edward-White19 <edward.white19@studytafensw.edu.au>
2024-12-02 11:30:29 +1100

    Swap the copyright to the original order before the pull request, because I seriously can't be bothered to take all the screenshots again.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -18,7 +18,7 @@ export default function HomeScreen(props) {
         <Divider horizontalInset={true} style={{ width: '100%' }} />
         <View style={styles.viewCopyright}>
           <Text variant='bodySmall' style={styles.textCopyright}>Developed by Edward White</Text>
-          <Text variant='bodySmall' style={styles.textCopyright}>&copy; 2024 Red Opal Innovations</Text>
+          <Text variant='bodySmall' style={styles.textCopyright}>Red Opal Innovations &copy; 2024</Text>
         </View>
       </Surface>
     </RoiBackdrop>
